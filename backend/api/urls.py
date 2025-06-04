6609f9805a36b141852ef53090f45d8727b3d102
sohan reddy Julakanti <sohanreddy99999@gmail.com>
2025-06-04 23:08:35 +0530

    fixed the form in trips page and modified courses page

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CourseListView, TripListView 
+from .views import CourseListView, TripListView , TripRegistrationView
 
 urlpatterns = [
     path("courses/", CourseListView.as_view(), name="courses"),  
     path("trips/", TripListView.as_view(), name="trips"),  # ✅ Use `.as_view()`
+    path("trips-registration/", TripRegistrationView.as_view(), name="trips-registration"),
 ]
