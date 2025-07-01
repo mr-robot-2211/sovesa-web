732baba1d3799fdc3044b895e8be4dbf05ecd361
AshArl <email>
2025-07-01 13:57:49 +0530

    resolved backend conflict

--- backend/custom_auth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignupView, LoginView, PostSadhanaView
-
-urlpatterns = [
-    path("signup/", SignupView.as_view(), name="signup"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("post-sadhana/", PostSadhanaView.as_view(), name="post-sadhana"),
-]
